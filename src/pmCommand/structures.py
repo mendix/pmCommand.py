cee1cc1aed4e3e09919e33ae6a9e597677431138
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2014-01-11 21:39:48 +0100

    extract load_array to load data from the xml

--- src/pmCommand/structures.py
+++ src/pmCommand/structures.py
@@ -9,10 +9,7 @@ class PDU(object):
     headers = {}
 
     def __init__(self, et_pdu):
-        self.text = {}
-        self.label = {}
-
-        for them, us in [
+        mapping = [
             ("pduId_Index", "name"),
             ("pdu_vendor", "vendor"),
             ("pdu_model", "model"),
@@ -22,15 +19,26 @@ class PDU(object):
             ("pdu_current", "current"),
             ("pdu_power", "power"),
             ("pdu_alarm", "alarm"),
-        ]:
-            et_param = et_pdu.find("./parameter[@id='%s']" % them)
-            et_value = et_param.find("./value")
+        ]
+        (self.text, self.label) = load_array(et_pdu, PDU.headers, mapping)
 
-            self.text[us] = et_value.text
-            self.label[us] = et_value.get("label")
 
-            header = et_param.get("label")
-            current_header = PDU.headers.setdefault(us, header)
-            if current_header != header:
-                logger.warn("PDU: different header for new row detected: %s -> %s"
-                            % (current_header, header))
+def load_array(et_array, headers, mapping):
+
+    text = {}
+    label = {}
+
+    for them, us in mapping:
+        et_param = et_array.find("./parameter[@id='%s']" % them)
+        et_value = et_param.find("./value")
+
+        text[us] = et_value.text
+        label[us] = et_value.get("label")
+
+        header = et_param.get("label")
+        current_header = headers.setdefault(us, header)
+        if current_header != header:
+            logger.warn("Different header for new row detected: %s -> %s"
+                        % (current_header, header))
+
+    return (text, label)
