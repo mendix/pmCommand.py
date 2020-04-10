0d401bf6723be9bc32f7dfb496d1d7e63113ac34
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2020-04-10 15:36:48 +0200

    Monkey patch logging instead of subclassing
    Instead of creating a subclass of the python logger, monkey patch the
    python logging to add a trace method.
    
    After doing so, we can stop passing around a modified logger object.

--- src/pmCommand/__init__.py
+++ src/pmCommand/__init__.py
@@ -1,6 +1,29 @@
+import logging
 import pmCommand.client  # noqa
 import pmCommand.util  # noqa
 from pmCommand.core import PMCommand  # noqa
 from pmCommand.log import logger  # noqa
 
 __version__ = 'UNRELEASED'
+
+
+def monkeypatch_logging():
+    # register trace logging possibility
+    TRACE = 5
+    logging.addLevelName(TRACE, 'TRACE')
+    setattr(logging, 'TRACE', TRACE)
+
+    def loggerClassTrace(self, msg, *args, **kwargs):
+        if self.isEnabledFor(TRACE):
+            self._log(TRACE, msg, args, **kwargs)
+
+    setattr(logging.getLoggerClass(), 'trace', loggerClassTrace)
+
+    def rootTrace(msg, *args, **kwargs):
+        if logging.root.isEnabledFor(TRACE):
+            logging.root._log(TRACE, msg, args, **kwargs)
+    setattr(logging, 'trace', rootTrace)
+
+
+if not hasattr(logging, 'trace'):
+    monkeypatch_logging()
