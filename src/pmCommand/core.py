mendix/pmCommand.py

commit 94b6468cb20c909b53f7df8412d5e3c544954a9f

Rewrite all the structures stuff
Ok, this is a pretty big change, but I can't really split it up and have
a working program on each intermediate commit without a lot of overhead.

Basically, I didn't really like how I did this in 2014, and it contained
a few things which would really be in the way of having this tool deal
with multiple PDUs to log into instead of just one PDU or ACS.

Changes:
- No custom mappings for field names, let's just use the ones from the
avocent xml stuff. This will make it a bit easier to debug when someone
else starts reverse engineering more things. This also makes the PDU and
Outlet class a lot simpler.
- Add a structures.DeviceFromXML class as super class for PDU and
Outlet, which contains the generic logic to just tetris field names,
column headers and actual values into the object. This replaces the old
load_array function in the module.
- No more class variables, so that we can later have multiple PMCommand
objects which use the PDU and Outlet structures. (maybe even combining
PDUs seen via an ACS with directly logging into another extra PDU)...
- Just use header and value instead of field, label, header, text, or
wherever all of the confusing names where it was stored before.
- Move print_table from cli to util.
- In Outlet, we previously already combined the pdu_id with
outlet_number to get the combined value in the outlet column (e.g.
'power2-213[3]'. This is the format that the outlet commands accept as
args (e.g. 'off power2-213[3],power4-213[3]'). Since we now just store
the attribute names and values as we get them from the xml, I added
output filter functionality to the print_table function, which can
provide a function to apply on the value of a field just before
displaying it.

Fixes: https://github.com/mendix/pmCommand.py/pull/3

src/pmCommand/core.py
@@ -3,7 +3,6 @@
 
 import logging
 from pmCommand.client import ACSClient
-from pmCommand.structures import PDU, Outlet
 
 
 class PMCommand():
@@ -23,31 +22,29 @@ class PMCommand():
     def listipdus(self):
         return self.client.listipdus()
 
-    def listipdus_table_info(self):
-        return PDU.fields, PDU.headers
-
     def status(self, outlet_list=None):
-        outlets = []
+        """
+        The arg outlet_list is either omitted (None) or a list of tuples with
+        (pdu_id, outlet_number), like ('power3', '7').
+
+        The return value is a dictionary which has the same kind of tuples as
+        keys and the actual Outlet objects as values.
+        """
+        result = {}
         if outlet_list is None:
             pdus = self.client.listipdus()
             for pdu in pdus:
-                pdu_id = pdu.text['name']
-                outlets.extend(self.client.outlets(pdu_id))
+                for outlet in self.client.outlets(pdu.pduId_Index):
+                    result[(pdu.pduId_Index, outlet.outlet_number)] = outlet
         else:
-            too_many_outlets = []
+            # which pdu collection do we need outlet info from?
             pdu_ids = set([_[0] for _ in outlet_list])
+            # for each pdu, get all outlets and only add requested ones to result
             for pdu_id in pdu_ids:
-                too_many_outlets.extend(self.client.outlets(pdu_id))
-            for pdu_id, outlet_id in outlet_list:
-                outlets.extend([
-                    _ for _ in too_many_outlets
-                    if _.text['outlet'] == '{}[{}]'.format(pdu_id, outlet_id)
-                ])
-
-        return outlets
-
-    def status_table_info(self):
-        return Outlet.fields, Outlet.headers
+                for outlet in self.client.outlets(pdu_id):
+                    if (pdu_id, outlet.outlet_number) in outlet_list:
+                        result[(pdu_id, outlet.outlet_number)] = outlet
+        return result
 
     def on(self, pdu_id, outlet_id):
         return self.outlet_action("on", pdu_id, outlet_id)

src/pmCommand.py
@@ -50,17 +50,24 @@ class CLI(cmd.Cmd):
         logging.info("Sorted output is now {}.".format("on" if self._sort else "off"))
 
     def do_listipdus(self, args):
-        pdus = self._pmCommand.listipdus()
-        fields, headers = self._pmCommand.listipdus_table_info()
-        self._print_table(fields, headers, pdus)
+        pmCommand.util.print_table(self._pmCommand.listipdus(), self._sort)
 
     def do_status(self, args):
         if args == "" or args == "all":
             outlets = self._pmCommand.status()
         else:
             outlets = self._pmCommand.status(self.parse_outlet_args(args))
-        fields, headers = self._pmCommand.status_table_info()
-        self._print_table(fields, headers, outlets)
+
+        def outlet_number_filter(device, attr):
+            # apply output filter on the outlet_number column to instead
+            # display the combined pdu_id[outlet_number], which is copy
+            # pastable as input arg for outlet actions like on, off, lock etc.
+            return "{}[{}]".format(device.pdu_id, device.values[attr])
+
+        pmCommand.util.print_table(
+            outlets.values(), self._sort,
+            output_filters={'outlet_number': outlet_number_filter},
+        )
 
     def outlet_action(self, action, args):
         for pdu_id, outlet_id in self.parse_outlet_args(args):
@@ -95,33 +102,6 @@ class CLI(cmd.Cmd):
 
     def do_save(self, args):
         self._pmCommand.save()
-
-    def _print_table(self, fields, headers, rows):
-        if self._sort:
-            rows = sorted(rows)
-
-        print()
-
-        output = ['']
-        maxlen = {}
-        for field in fields:
-            maxlen[field] = max(len(headers[field]),
-                                max([len(row.label[field]) for row in rows]))
-            output.append(headers[field].ljust(maxlen[field]))
-        print('  '.join(output))
-
-        output = ['']
-        for field in fields:
-            output.append('=' * maxlen[field])
-        print('  '.join(output))
-
-        for row in rows:
-            output = ['']
-            for field in fields:
-                output.append(row.label[field].ljust(maxlen[field]))
-            print('  '.join(output))
-
-        print()
 
     def do_exit(self, args):
         self.do_logout(None)

src/pmCommand/structures.py
@@ -1,80 +1,72 @@
 # Copyright 2014 Mendix
 # MIT license, see LICENSE, see LICENSE
 
-import logging
+
+class DeviceFromXML:
+    def __init__(self, et_array):
+        self.headers = {}  # description, useful as column header
+        self.values = {}
+
+        for et_param in et_array.findall("./parameter"):
+            param_id = et_param.get("id")  # e.g. pduId_Index
+            header = et_param.get("label")
+            if header is None:
+                continue  # hidden, e.g. the 'thresholds' field of an ipdu
+            self.headers[param_id] = et_param.get("label")
+            et_value = et_param.find("./value")
+            self.values[param_id] = et_value.get("label")
+
+    def __getattr__(self, attr):
+        if attr in self.values:
+            return self.values[attr]
+        else:
+            raise AttributeError
 
 
-class PDU:
+class PDU(DeviceFromXML):
+    """
+    Load PDU information from an XML PDU object
 
-    fields = 'name', 'vendor', 'model', 'position', 'status', \
-        'outlets', 'current', 'power', 'alarm'
+    Known attributes (all are stored as text):
 
-    headers = {}
-
+    Attribute     Header              Example value
+    =========     ===========         =======
+    pduId_Index   PDU ID              power3-r17
+    pdu_vendor    Vendor              Avocent
+    pdu_model     Model               PM3000/10/16A
+    pdu_pos       Position            ttyS4/1
+    pdu_status    Status              On Line
+    pdu_outlets   Outlets (On/Total)  8/10
+    pdu_current   Current (A)         4.2
+    pdu_power     Power (W)           1002.0
+    pdu_alarm     Alarm               Normal
+    """
     def __init__(self, et_pdu):
-        mapping = [
-            ("pduId_Index", "name"),
-            ("pdu_vendor", "vendor"),
-            ("pdu_model", "model"),
-            ("pdu_pos", "position"),
-            ("pdu_status", "status"),
-            ("pdu_outlets", "outlets"),
-            ("pdu_current", "current"),
-            ("pdu_power", "power"),
-            ("pdu_alarm", "alarm"),
-        ]
-        self.text, self.label = load_array(et_pdu, PDU.headers, mapping)
+        super().__init__(et_pdu)
 
     def __lt__(self, other):
-        return self.text['name'] < other.text['name']
+        return self.pduId_Index < other.pduId_Index
 
 
-class Outlet:
+class Outlet(DeviceFromXML):
+    """
+    Load Outlet information
 
-    fields = 'outlet', 'name', 'status', 'current'
+    Known attributes (all are stored as text):
 
-    headers = {}
-
+    Attribute        Header         Example value
+    =========        ===========    =======
+    outlet_number    Outlet         6
+    outlet_name      Name           HE20-flapsie-L
+    status           Status         ON(locked)
+    circuit          Bank           N/A
+    outlet_current   Current (A)    0.4
+    outlet_power     Power (W)      110.0
+    """
     def __init__(self, et_outlet, pdu_id):
-        mapping = [
-            ("outlet_number", "number"),
-            ("outlet_name", "name"),
-            ("status", "status"),
-            ("outlet_current", "current"),
-            ("outlet_power", "power"),
-        ]
-        self.text, self.label = load_array(et_outlet, Outlet.headers, mapping)
-
-        Outlet.headers['outlet'] = "Outlet"
-        self.text['outlet'] = "%s[%s]" % (pdu_id, self.text['number'])
-        self.label['outlet'] = self.text['outlet']
-
-        self.text['name'] = self.label['name']
-        self.label['name'] = self.text['name']
-
-        self.pdu_id = pdu_id
+        super().__init__(et_outlet)
+        self.pdu_id = pdu_id  # for sorting purposes
 
     def __lt__(self, other):
-        return (self.pdu_id, int(self.text['number'])) < \
-            (other.pdu_id, int(other.text['number']))
-
-
-def load_array(et_array, headers, mapping):
-
-    text = {}
-    label = {}
-
-    for them, us in mapping:
-        et_param = et_array.find("./parameter[@id='%s']" % them)
-        et_value = et_param.find("./value")
-
-        text[us] = et_value.text
-        label[us] = et_value.get("label")
-
-        header = et_param.get("label")
-        current_header = headers.setdefault(us, header)
-        if current_header != header:
-            logging.warn("Different header for new row detected: {} -> {}".format(
-                current_header, header))
-
-    return text, label
+        return (self.pdu_id, int(self.outlet_number)) < \
+            (other.pdu_id, int(other.outlet_number))

src/pmCommand/util.py
@@ -1,6 +1,7 @@
 # Copyright 2014 Mendix
 # MIT license, see LICENSE, see LICENSE
 
+import logging
 import pmCommand
 import re
 
@@ -13,3 +14,50 @@ def parse_outlet(pdu_outlet):
         raise pmCommand.Error("Invalid outlet: {}".format(pdu_outlet))
     groupdict = match.groupdict()
     return groupdict['pdu_id'], groupdict['outlet_id']
+
+
+def print_table(devices, sort=True, output_filters=None):
+    if output_filters is None:
+        output_filters = {}
+
+    def apply_output_filter(device, attr):
+        if attr not in output_filters:
+            return device.values[attr]
+        return output_filters[attr](device, attr)
+
+    if len(devices) == 0:
+        logging.info("Empty result, nothing to show.")
+        return
+
+    if sort:
+        devices = sorted(devices)
+
+    print()
+
+    output = ['']
+    maxlen = {}
+
+    # Just assume all objects in devices are of equal type (like, all PDUs or
+    # all Outlets and have the same fields to show. Use the field names from
+    # the first one to look at all column headers and actual values to find the
+    # longest one.
+    attrs = devices[0].headers.keys()
+    for attr in attrs:
+        maxlen[attr] = max(len(devices[0].headers[attr]),
+                           max([len(apply_output_filter(device, attr))
+                                for device in devices]))
+        output.append(devices[0].headers[attr].ljust(maxlen[attr]))
+    print('  '.join(output))
+
+    output = ['']
+    for attr in attrs:
+        output.append('=' * maxlen[attr])
+    print('  '.join(output))
+
+    for device in devices:
+        output = ['']
+        for attr in attrs:
+            output.append(apply_output_filter(device, attr).ljust(maxlen[attr]))
+        print('  '.join(output))
+
+    print()

commit 89610db057300f78b14d7b3b5a8baab762e421ba

core: fix AttributeError in save
This was broken since commit 94b6468cb20c ("Rewrite all the structures
stuff")

src/pmCommand/core.py
@@ -66,8 +66,8 @@ class PMCommand():
 
     def save(self):
         for pdu in self.client.listipdus():
-            logging.info("Saving configuration on PDU {}.".format(pdu.text['name']))
-            self.client.save(pdu.text['name'])
+            logging.info("Saving configuration on PDU {}.".format(pdu.pduId_Index))
+            self.client.save(pdu.pduId_Index)
 
     def get_session_idle_timeout(self):
         return self.client.get_session_idle_timeout()